e2a32bdde0f1ba18ad8e17108efec4d7286d4c2d
KIRIN <103432491+kirinyoku@users.noreply.github.com>
2023-12-05 14:38:30 +0200

    feat: add perks data

---
+++ src/config/perks.ts
@@ -0,0 +1,24 @@
+import { ArrowDownToLine, CheckCircle, Leaf } from "lucide-react";
+
+const perks = [
+  {
+    name: "Instant Delivery",
+    Icon: ArrowDownToLine,
+    description:
+      "Get your assets delivered to your email in seconds and download them right away.",
+  },
+  {
+    name: "Guaranteed Quality",
+    Icon: CheckCircle,
+    description:
+      "Every asset on our platform is veryfied by our team to ensure quality standards.",
+  },
+  {
+    name: "For the Planet",
+    Icon: Leaf,
+    description:
+      "We've pledge 1% of sales to the preservation and restoration of the natural environment.",
+  },
+];
+
+export default perks;
