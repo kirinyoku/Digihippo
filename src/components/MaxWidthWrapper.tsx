fe886a4d545025bad3a0b599dc686490065e6d8d
KIRIN <103432491+kirinyoku@users.noreply.github.com>
2023-12-05 13:24:58 +0200

    feat(layout): add MaxWidthWrapper component

---
+++ src/components/MaxWidthWrapper.tsx
@@ -0,0 +1,22 @@
+import { cn } from "@/lib/utils";
+
+interface MaxWidthWrapperProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+export default function MaxWidthWrapper({
+  children,
+  className,
+}: MaxWidthWrapperProps) {
+  return (
+    <div
+      className={cn(
+        "mx-auto w-full max-w-screen-xl px-2.5 md:px-20",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+}
