a5996e692fe7c5405946b44c4d394b6bdaf8af02
Kirin <rudenokaleksandr21@gmail.com>
2024-01-03 19:16:11 +0200

    (refactor): split server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,73 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import path from "path";
-import payload, { Payload } from "payload";
-import next from "next";
-import type { InitOptions } from "payload/config";
 import * as trpcExpress from "@trpc/server/adapters/express";
 import { appRouter } from "./trpc";
-import nodemailer from "nodemailer";
+import { nextApp, nextHandler } from "./nextUtils";
 import { inferAsyncReturnType } from "@trpc/server";
+import { getPayloadClient } from "./getPayload";
 
 dotenv.config({
   path: path.resolve(__dirname, "../.env"),
 });
-
-const transporter = nodemailer.createTransport({
-  host: "smtp.resend.com",
-  secure: true,
-  port: 465,
-  auth: {
-    user: "resend",
-    pass: process.env.RESEND_API_KEY,
-  },
-});
-
-let cached = (global as any).payload;
-
-if (!cached) {
-  cached = (global as any).payload = {
-    client: null,
-    promise: null,
-  };
-}
-
-interface Args {
-  initOptions?: Partial<InitOptions>;
-}
-
-export async function getPayloadClient({
-  initOptions,
-}: Args = {}): Promise<Payload> {
-  if (!process.env.PAYLOAD_SECRET) {
-    throw new Error("PAYLOAD_SECRET is missing");
-  }
-
-  if (cached.client) {
-    return cached.client;
-  }
-
-  if (!cached.promise) {
-    cached.promise = payload.init({
-      email: {
-        transport: transporter,
-        fromAddress: "onboarding@resend.com",
-        fromName: "DigiHippo",
-      },
-      secret: process.env.PAYLOAD_SECRET || "",
-      local: initOptions?.express ? false : true,
-      ...(initOptions || {}),
-    });
-  }
-
-  try {
-    cached.client = await cached.promise;
-  } catch (error: unknown) {
-    throw error;
-  }
-
-  return cached.client;
-}
 
 const app = express();
 const PORT = Number(process.env.PORT) || 3000;
@@ -78,13 +20,6 @@ function createContext({ req, res }: trpcExpress.CreateExpressContextOptions) {
     res,
   };
 }
-
-const nextApp = next({
-  dev: process.env.NODE_ENV !== "production",
-  port: PORT,
-});
-
-const nextHandler = nextApp.getRequestHandler();
 
 async function start() {
   const payload = await getPayloadClient({
