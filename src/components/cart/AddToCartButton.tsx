f6582a74f07b080bd6b1aa909b3c9d647faceee2
Kirin <rudenokaleksandr21@gmail.com>
2024-01-08 10:16:09 +0200

    (refactor): move the cart components to a separate dir

---
+++ src/components/cart/AddToCartButton.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useCart } from "@/hooks/useCart";
+import { Product } from "@/config/payload-types";
+import { Button } from "../ui/button";
+
+export default function AddToCartButton({ product }: { product: Product }) {
+  const { addItem } = useCart();
+  const [isSuccess, setIsSuccess] = useState<boolean>(false);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsSuccess(false);
+    }, 2000);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [isSuccess]);
+
+  return (
+    <Button
+      onClick={() => {
+        addItem(product);
+        setIsSuccess(true);
+      }}
+      size="lg"
+      className="w-full"
+    >
+      {isSuccess ? "Added" : "Add to cart"}
+    </Button>
+  );
+}
