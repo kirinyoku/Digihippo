38c39ef8b54a361895375382ddcc85a86c3ecc92
KIRIN <103432491+kirinyoku@users.noreply.github.com>
2024-01-09 18:27:08 +0200

    feat: add footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { cn, constructMetadata } from "@/lib/utils";
 import Navbar from "@/components/navbar/Navbar";
 import Providers from "@/components/Providers";
 import { Toaster } from "sonner";
+import Footer from "@/components/footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,6 +25,7 @@ export default function RootLayout({
           <Providers>
             <Navbar />
             <div className="flex-grow flex-1">{children}</div>
+            <Footer />
           </Providers>
         </main>
         <Toaster position="top-center" richColors />
